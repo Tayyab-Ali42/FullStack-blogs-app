5d02ae0c9c7a39affa957fd5dd6260a5bbd2265e
Tayyab Ali <freelancewithtayyab@gmail.com>
2026-02-26 16:42:58 +0500

    added login functionality

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -33,3 +33,30 @@ export const registerUser = async (req, res) => {
         return res.status(500).json({ message: "Server error", error })
     }
 }
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+
+        const existUser = await User.findOne({ email })
+
+        if (!existUser) {
+            return res.status(401).json({ message: "Invalid credentials" })
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, existUser.password)
+
+        if (!isPasswordMatch) {
+            return res.status(401).json({ message: "Invalid credentials" })
+        }
+
+        return res.status(200).json({ message: "User successfully loggedIn", })
+
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error })
+    }
+}
