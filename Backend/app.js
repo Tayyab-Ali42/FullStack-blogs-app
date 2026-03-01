c1df4e381389667080630440ee9f80128077e9d6
Tayyab Ali <freelancewithtayyab@gmail.com>
2026-03-01 16:21:36 +0500

    setup comment routes

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from "cors"
 import userRoutes from "./routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
+import commentRoutes from "./routes/commentsRoutes.js"
 
 const app = express()
 
@@ -13,7 +14,8 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.use("/auth", userRoutes)
-app.use("/post", postRoutes)
+app.use("/api", postRoutes)
+app.use("/posts", commentRoutes)
 
 
 export default app
